e55a3d695e5362c37ec12b89c03ca4bc261eab6e
MPinna <marco.pinn95@gmail.com>
2021-05-21 17:58:58 +0200

    Minor fixes to script and excel file

--- bench.py
+++ bench.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 import io
-from os import PathLike, close, path
+# from os import PathLike, close, path
+import os
 import subprocess
 from datetime import datetime
-from os import listdir, system
+import exiftool
+# from os import listdir, system
 
-system("sudo fan_up.sh")
-for filename in listdir("videos/"):
+
+#TODO: add check for operating system
+os.system("sudo fan_up.sh")
+for filename in os.listdir("videos/"):
     f = open("log/"+filename+"_LOG.txt", "w")
+
+    # TODO: tweak verbosity level
     res=subprocess.call(
         ['HandBrakeCLI', '--preset-import-file','HB_presets/x264_MKV_1080p30_med_24.json', 
             '-i', "videos/"+filename, '-o', "encoded/"+filename[:-5]+"enc_.mkv"],
-        stdout=f, stderr=f)
-system("sudo fan_down.sh")
+        stdout = None, stderr = None)
+    # TODO retrieve size using exiftool
+    # os.path.getsize
+    metadata = exiftool.ExifTool().get_metadata(filename)
+    metadata['framerate']
+os.system("sudo fan_down.sh")
