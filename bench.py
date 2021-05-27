a22dd982db827e1e7a0f773a2994f70d296feef9
MPinna <marco.pinn95@gmail.com>
2021-05-27 21:45:20 +0200

    Add encoding check to script and metadata output

--- bench.py
+++ bench.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 import time
 import exiftool
+import json
 
 # we only read from this folder
 INPUT_FOLDER="videos/"
@@ -12,6 +13,8 @@ INPUT_FOLDER="videos/"
 # in order to preserve your SSD TBW
 # OUTPUT_FOLDER="/run/media/yuri/Dati/hw_bench/"
 OUTPUT_FOLDER="./output/"
+
+encoding = "SW" # CHANGE ACCORDINGLY
 
 def main():
     current_platform = platform.system()
@@ -24,23 +27,36 @@ def main():
         print("Turning fans on...")
         #os.system("sudo fan_up.sh")
 
-    input_metadata_array =  []
-    output_metadata_array =  []
     times = []
 
     for i, filename in enumerate(os.listdir(INPUT_FOLDER)):
 
         input_path = INPUT_FOLDER + filename
-        output_path = OUTPUT_FOLDER + filename[:-5] + "enc1_.mkv"
+        output_path = OUTPUT_FOLDER + filename[:-5]
 
-        f = open(OUTPUT_FOLDER + "log/"+filename[:-5] + ".log", "w+")
+        metadata_filename = filename[:-5]
+
+
+        encoding_check = ((encoding == "SW") | (encoding == "HW"))
+        assert encoding_check, "Error: unknow encoding. Make sure you initialized the variable 'encoding' correctly."
+        
+        if(encoding == "SW"):
+            output_path += "enc1.mkv"
+            f = open(OUTPUT_FOLDER + "log/"+filename[:-5] + "enc1.log", "w+")
+            metadata_filename +=  "1.json"
+
+        elif(encoding == "HW"):
+            output_path += "enc2.mkv"
+            f = open(OUTPUT_FOLDER + "log/"+filename[:-5] + "enc2.log", "w+")
+            metadata_filename +=  "2.json"
+
         # TODO: tweak verbosity level
         print("Processing file " + str(i + 1) + "/" + str(len(os.listdir(INPUT_FOLDER))) + " : " + filename)
 
         t0 = time.time()
 
         res=subprocess.call(
-        [ handbrake_path , '--preset-import-file','HB_presets/swdef.json', 
+        [ handbrake_path , '--preset-import-file','HB_presets/hwdef.json', 
                 '-i', input_path, '-o', output_path]
                 # important log messages are in stderr, we can throw away stdout
                 ,stderr=f, stdout=subprocess.DEVNULL)
@@ -54,9 +70,12 @@ def main():
 
             # (video duration, time needed, starting dimension, ending dimension, avg framerate)
         # in csv or json format
+
         with exiftool.ExifTool() as et:
-                input_metadata_array.append(et.get_metadata(input_path))
-                output_metadata_array.append(et.get_metadata(output_path))
+                metadata = et.get_metadata(output_path)
+
+                with open(OUTPUT_FOLDER + "metadata/" + metadata_filename, "w") as outfile:
+                    json.dump(metadata, outfile)
 
     if(current_platform == "Linux"):
         print("Shutting fans down...")
